9ed7264fb5a9f50dbec00bdc93c85315c61cdb09
Felipe Borba <felipe.borba0109@gmail.com>
2024-05-17 20:33:19 -0300

    fix migration

--- api/prisma/migrations/20240517233256_create_auth/migration.sql
+++ api/prisma/migrations/20240517233256_create_auth/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE `Auth` (
     `id` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
+    `cpf` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Auth_username_key`(`username`),
+    UNIQUE INDEX `Auth_cpf_key`(`cpf`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
