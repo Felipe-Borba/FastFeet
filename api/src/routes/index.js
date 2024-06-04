19679ef8414bc150359bdad526434fc99c4ce875
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-04 19:36:38 -0300

    test insomnia

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
-const userRoutes = require("./user.routes");
-const parcelRoutes = require("./parcel.routes");
+// const userRoutes = require("./user.routes");
+// const parcelRoutes = require("./parcel.routes");
 
 const routes = Router();
 
 // Rotas dos controllers
 // routes.use("/user", userRoutes);
-routes.use("/parcel", parcelRoutes)
+// routes.use("/parcel", parcelRoutes)
+routes.get("/hello-world", (request, response) => {
+  response.json({ message: "Hello there" });
+});
 
 module.exports = routes;
