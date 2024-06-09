154ecab3ff60be32c62327ba2fac948e15ac63f2
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-09 16:31:28 -0300

    config router

---
+++ web/src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div id="home">
+      <h1>Vite + React</h1>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
