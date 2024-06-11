37c4b4831326682cf15c0d60af89ff0d4cf50b42
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-11 19:13:19 -0300

    feat: create frontend pages

---
+++ web/src/pages/admin/index.jsx
@@ -0,0 +1,15 @@
+import "./deliveryMan.css";
+
+export default function Home() {
+  return (
+    <div id="delivery-man">
+      <h1>Vite + React</h1>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
---
+++ web/src/pages/login/index.jsx
@@ -0,0 +1,15 @@
+import "./home.css";
+
+export default function Login() {
+  return (
+    <div id="login">
+      <h1>Vite + React</h1>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
