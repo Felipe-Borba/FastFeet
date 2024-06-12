85e0ed432d82c943a41174a4f654824ca789f0b6
ANDRE LUIS FERNANDES DO ESPIRITO SANTO <andre_santo@estudante.sc.senai.br>
2024-06-11 21:37:50 -0300

    Pagina web(home)

---
+++ web/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+export default function Header(){
+    return(
+        <header id="header">
+            <div className="logo">
+                <img src=".\src\assets\FastFeetLogo2.png" alt="logo"/>
+            </div>
+        </header>
+    )
+}
