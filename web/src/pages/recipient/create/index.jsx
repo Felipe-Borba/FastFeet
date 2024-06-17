5e227697c6d6d4e3e686e52375f4fbcd6d17a1ac
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-16 22:06:06 -0300

    crud recipient

--- web/src/pages/recipient/create/index.jsx
+++ web/src/pages/recipient/create/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../../services/api";
 
 const CreateRecipient = () => {
-  return <div>CreateRecipient</div>;
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+
+    await api.post("/recipient", { name });
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleCreate}>
+        <label>
+          Nome
+          <input value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipient;
