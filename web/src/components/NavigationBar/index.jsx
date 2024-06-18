fca14e403cbdb0844615e89443bb4191f9c1dc4f
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-18 20:24:49 -0300

    fix layout

--- web/src/components/NavigationBar/index.jsx
+++ web/src/components/NavigationBar/index.jsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 const NavigationBar = ({ selected }) => {
   return (
-    <navigation className="p-10 flex flex-col gap-2 h-full">
+    <navigation className="p-10 flex flex-col gap-2 h-full bg-gray-400">
       <Button to={"/user/create"} selected={selected}>
         Criar usuário
       </Button>
@@ -24,8 +24,8 @@ const Button = ({ children, to, selected, className }) => {
     <Link
       to={to}
       className={twMerge(
-        "p-4 rounded-md",
-        selected ? "border border-yellow-300" : "",
+        "p-4 rounded-md bg-gray-100 text-center text-black font-bold",
+        selected == to ? "border border-yellow-300" : "",
         className
       )}
     >
