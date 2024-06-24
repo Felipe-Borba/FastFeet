f57be5fddfecd8d5dbddbb330b56541dd9693b1c
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-23 23:47:08 -0300

    feat: update recipient

--- web/src/pages/recipient/list/index.jsx
+++ web/src/pages/recipient/list/index.jsx
@@ -10,6 +10,19 @@ import {
 import { useEffect, useState } from "react";
 import LayoutMain from "../../../components/LayoutMain";
 import { api } from "../../../services/api";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/src/components/ui/dialog";
+import { Button } from "@/src/components/ui/button";
+import { Label } from "@/src/components/ui/label";
+import { Input } from "@/src/components/ui/input";
 
 const ListRecipient = () => {
   const [recipient, setRecipient] = useState([]);
@@ -53,11 +66,36 @@ const ListRecipient = () => {
               <TableRow key={item.id}>
                 <TableCell>{item.name}</TableCell>
                 <TableCell>
-                  <DeleteButton
-                    onContinue={() => {
-                      deleteRecipient(item.id);
-                    }}
-                  />
+                  <div className="flex gap-3 justify-center">
+                    <DeleteButton
+                      onContinue={() => {
+                        deleteRecipient(item.id);
+                      }}
+                    />
+
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button variant="outline">Atualizar</Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Atualizar usuário</DialogTitle>
+                          <DialogDescription>
+                            Depois de atualizar o usuário clique em salvar
+                          </DialogDescription>
+                        </DialogHeader>
+                        <UserForm formId={"update"} user={item} />
+                        <DialogFooter>
+                          <DialogClose>
+                            <Button>Cancelar</Button>
+                          </DialogClose>
+                          <Button form="update" type="submit">
+                            Salvar
+                          </Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
                 </TableCell>
               </TableRow>
             );
@@ -69,3 +107,35 @@ const ListRecipient = () => {
 };
 
 export default ListRecipient;
+
+const UserForm = ({ user, formId, preventDefault = false }) => {
+  const [name, setName] = useState(user?.name ?? "");
+
+  const updateUser = async () => {
+    try {
+      await api.put("/recipient", { id: user.id, name });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    if (preventDefault) {
+      event.preventDefault();
+    }
+    await updateUser();
+  };
+
+  return (
+    <form id={formId} onSubmit={handleSubmit}>
+      <Label>
+        Nome
+        <Input
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Label>
+    </form>
+  );
+};
