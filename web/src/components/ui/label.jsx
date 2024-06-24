2ecfa6405a335ec45d573cf683a7a548d99de3cd
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-23 21:48:26 -0300

    fix create user form

--- web/src/components/ui/label.jsx
+++ web/src/components/ui/label.jsx
@@ -1,16 +1,20 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva } from "class-variance-authority";
 
-import { cn } from "@/src/lib/utils"
+import { cn } from "@/src/lib/utils";
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 flex-col gap-1"
+);
 
 const Label = React.forwardRef(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
 
-export { Label }
+export { Label };
--- web/src/pages/user/userCreate/index.jsx
+++ web/src/pages/user/userCreate/index.jsx
@@ -1,53 +1,78 @@
-import Header from "../../../components/header";
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+import { Label } from "@/src/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/components/ui/select";
 import { useState } from "react";
-import "./user.css";
-import { api } from "../../../services/api";
+import { useNavigate } from "react-router-dom";
 import Layout from "../../../components/LayoutMain";
-import InputSelect from "../../../components/inputSelect";
+import { api } from "../../../services/api";
+import "./user.css";
 
 export default function UserCreate() {
   const [name, setName] = useState("");
   const [cpf, setCpf] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
+  const navigate = useNavigate();
 
-  const handleuserCreate = async (e) => {
+  const handleUserCreate = async (e) => {
     e.preventDefault();
     try {
-      const response = await api.post("/user", { name, cpf, password, role });
-      //TODO add header api.defaults.headers
-      console.log(response.status);
-      // console.log(response.data);
-      //TODO redirecionar para a tela do usuário
+      await api.post("/user", { name, cpf, password, role });
+      navigate("/user/list");
     } catch (error) {
+      console.log(error.message);
       alert("Algum erro");
     }
-    console.log(99);
   };
+
   return (
     <Layout id="create-user" selected={"/user/create"}>
       <section>
         <h1 className="titulo-principal">Criar usuário</h1>
         <div id="UserCreateForm">
-          <form onSubmit={handleuserCreate}>
-            <label>
+          <form onSubmit={handleUserCreate}>
+            <Label>
               Nome
-              <input value={name} onChange={(e) => setName(e.target.value)} />
-            </label>
-            <label>
+              <Input value={name} onChange={(e) => setName(e.target.value)} />
+            </Label>
+
+            <Label>
               CPF
-              <input value={cpf} onChange={(e) => setCpf(e.target.value)} />
-            </label>
-            <label>
+              <Input value={cpf} onChange={(e) => setCpf(e.target.value)} />
+            </Label>
+
+            <Label>
               Senha
-              <input
+              <Input
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-            </label>
-            <InputSelect></InputSelect>
+            </Label>
+
+            <Select value={role} onValueChange={setRole}>
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione um perfil" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Perfil</SelectLabel>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="entregador">Entregador</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+
             <div className="button-container">
-              <button type="submit">Registrar</button>
+              <Button type="submit">Registrar</Button>
             </div>
           </form>
         </div>
