a1ec01d34eab563668afb8d0f4b40a84c94bd1cb
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-25 10:45:50 -0300

    api deploy pm2 config

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,26 +1,15 @@
 module.exports = {
   apps: [
     {
+      name: "api",
       script: "./src/server.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
 };
