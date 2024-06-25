0eb11d022b1309d4fb543fe28826d9208542b9dc
Felipe Borba <felipe.borba0109@gmail.com>
2024-06-25 16:52:53 -0300

    fix web env

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: process?.env?.REACT_BASE_URL ?? "http://localhost:3333",
+  baseURL: import.meta.env.BASE_URL,
 });
